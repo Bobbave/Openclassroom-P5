6fcc117235559f857cc0f4f6bd77bbb5eacc276f
Bobbave <gay.jordan.16@gmail.com>
2022-09-27 18:12:29 +0200

    Added products to index and set module.js

---
+++ front/js/script.js
@@ -0,0 +1,39 @@
+import { API_URL } from "../js/module.js";
+
+const apiGet = fetch (API_URL);
+
+apiGet
+    .then (async function (response){
+
+        let products = await response.json();
+
+        function getProducts(){
+            for (let i = 0; i < products.length; i++) {
+
+                const items = document.querySelector("#items");
+    
+                const link = document.createElement("a");
+    
+                const article = document.createElement("article");
+    
+                const itemImage = document.createElement("img");
+                itemImage.src = products[i].imageUrl;
+                itemImage.alt = products[i].altTxt;
+    
+                const name = document.createElement("h3");
+                name.textContent = products[i].name;
+
+                const description = document.createElement("p");
+                description.textContent = products[i].description;
+    
+                items.appendChild(link);
+                link.appendChild(article);
+                article.appendChild(itemImage);
+                article.appendChild(name);
+                article.appendChild(description);
+            }
+        }
+
+        getProducts();
+
+    })
